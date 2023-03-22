8ab430f9cad6ebe086ddfc9a6d3dc94889eee657
Arvind Ganesh <arvgan@Arvinds-MacBook-Pro.local>
2023-03-21 22:36:32 -0500

    Added PPO training script

---
+++ PPO_agent.py
@@ -0,0 +1,319 @@
+import argparse
+import datetime
+import os
+import pprint
+
+import numpy as np
+import torch
+from torch.optim.lr_scheduler import LambdaLR
+from torch.utils.tensorboard import SummaryWriter
+
+from tianshou.data import Collector, VectorReplayBuffer
+from tianshou.policy import ICMPolicy, PPOPolicy
+from tianshou.trainer import onpolicy_trainer
+from tianshou.utils import TensorboardLogger, WandbLogger
+from tianshou.utils.net.common import ActorCritic
+from tianshou.utils.net.discrete import Actor, Critic, IntrinsicCuriosityModule
+from configure_env import make_retro_env
+
+# TODO: Make this configurable from the command line
+class PolicyNet(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        # Input: 4x84x84, assumes frame stacking, resizing, and gray-scaling have been done.
+        self.network = torch.nn.Sequential(
+            torch.nn.Conv2d(4, 16, 8, stride=4), # 4x84x84 -> 16x20x20
+            torch.nn.ReLU(inplace=True),
+            torch.nn.Conv2d(16, 32, 4, stride=2), # 16x20x20 -> 32x9x9
+            torch.nn.ReLU(inplace=True),
+            torch.nn.AvgPool2d(2), # 32x9x9 -> 32x4x4
+            torch.nn.Flatten()
+        )
+        self.output_dim = 32 * 4 * 4 # Need this for Actor, Critic functions to work correctly.
+
+    def forward(self, obs, state=None, info={}):
+        if not isinstance(obs, torch.Tensor):
+            obs = torch.tensor(obs, dtype=torch.float)
+        logits = self.network(obs)
+        return logits, state
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--task", type=str, default="MarioKart-Snes")
+    parser.add_argument("--custom-integration-path", type=str, default=None)
+    parser.add_argument("--seed", type=int, default=4213)
+    parser.add_argument("--scale-obs", type=int, default=1)
+    parser.add_argument("--buffer-size", type=int, default=100000)
+    parser.add_argument("--lr", type=float, default=3e-4)
+    parser.add_argument("--gamma", type=float, default=0.99)
+    parser.add_argument("--epoch", type=int, default=100)
+    parser.add_argument("--step-per-epoch", type=int, default=100000)
+    parser.add_argument("--step-per-collect", type=int, default=1000)
+    parser.add_argument("--repeat-per-collect", type=int, default=4)
+    parser.add_argument("--batch-size", type=int, default=256)
+    parser.add_argument("--hidden-size", type=int, default=512)
+    parser.add_argument("--training-num", type=int, default=2)
+    parser.add_argument("--test-num", type=int, default=2)
+    parser.add_argument("--rew-norm", type=int, default=False)
+    parser.add_argument("--vf-coef", type=float, default=0.25)
+    parser.add_argument("--ent-coef", type=float, default=0.01)
+    parser.add_argument("--gae-lambda", type=float, default=0.95)
+    parser.add_argument("--lr-decay", type=int, default=True)
+    parser.add_argument("--max-grad-norm", type=float, default=0.5)
+    parser.add_argument("--eps-clip", type=float, default=0.1)
+    parser.add_argument("--dual-clip", type=float, default=None)
+    parser.add_argument("--value-clip", type=int, default=1)
+    parser.add_argument("--norm-adv", type=int, default=1)
+    parser.add_argument("--recompute-adv", type=int, default=0)
+    parser.add_argument("--logdir", type=str, default="log")
+    parser.add_argument("--render", type=float, default=0.)
+    parser.add_argument(
+        "--device", type=str, default="cuda" if torch.cuda.is_available() else "cpu"
+    )
+    parser.add_argument("--frame-stack", type=int, default=4)
+    parser.add_argument("--frame-skip", type=int, default=4)
+    parser.add_argument("--resume-path", type=str, default=None)
+    parser.add_argument("--resume-id", type=str, default=None)
+    parser.add_argument(
+        "--logger",
+        type=str,
+        default="wandb",
+        choices=["tensorboard", "wandb"],
+    )
+    parser.add_argument("--wandb-project", type=str, default="kart-snes.benchmark")
+    parser.add_argument(
+        "--watch",
+        default=False,
+        action="store_true",
+        help="watch the play of pre-trained policy only"
+    )
+    parser.add_argument("--save-buffer-name", type=str, default=None)
+
+    # TODO: Integrate Intrinsic Curiosity Module
+    # parser.add_argument(
+    #     "--icm-lr-scale",
+    #     type=float,
+    #     default=0.,
+    #     help="use intrinsic curiosity module with this lr scale"
+    # )
+    # parser.add_argument(
+    #     "--icm-reward-scale",
+    #     type=float,
+    #     default=0.01,
+    #     help="scaling factor for intrinsic curiosity reward"
+    # )
+    # parser.add_argument(
+    #     "--icm-forward-loss-weight",
+    #     type=float,
+    #     default=0.2,
+    #     help="weight for the forward model loss in ICM"
+    # )
+
+    return parser.parse_args()
+
+
+def test_ppo(args=get_args()):
+    env, train_envs, test_envs = make_retro_env(
+        args.task,
+        args.seed,
+        args.training_num,
+        args.test_num,
+        frame_stack=args.frame_stack,
+        frame_skip=args.frame_skip,
+        custom_integration_path=args.custom_integration_path
+    )
+
+    args.state_shape = env.observation_space[0].shape or env.observation_space[0].n
+    args.action_shape = env.action_space[0].shape or env.action_space[0].n
+
+    # should be N_FRAMES x H x W
+    print("Observations shape:", args.state_shape)
+    print("Actions shape:", args.action_shape)
+
+    # seed
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+
+    # define model
+    net = PolicyNet()
+    actor = Actor(net, args.action_shape, device=args.device, softmax_output=False) # softmax_output is False b/c of line 154.
+    critic = Critic(net, device=args.device)
+    optim = torch.optim.Adam(
+        ActorCritic(actor, critic).parameters(), lr=args.lr, eps=1e-5
+    )
+
+    lr_scheduler = None
+    if args.lr_decay:
+        # decay learning rate to 0 linearly
+        max_update_num = np.ceil(
+            args.step_per_epoch / args.step_per_collect
+        ) * args.epoch
+
+        lr_scheduler = LambdaLR(
+            optim, lr_lambda=lambda epoch: 1 - epoch / max_update_num
+        )
+
+    # Policy = Categorical Distribution parameterized by logits
+    def dist(p):
+        return torch.distributions.Categorical(logits=p) # logits are interpreted as unnormalized log-probs.
+
+    policy = PPOPolicy(
+        actor,
+        critic,
+        optim,
+        dist,
+        discount_factor=args.gamma,
+        gae_lambda=args.gae_lambda,
+        max_grad_norm=args.max_grad_norm,
+        vf_coef=args.vf_coef,
+        ent_coef=args.ent_coef,
+        reward_normalization=args.rew_norm,
+        action_scaling=False,
+        lr_scheduler=lr_scheduler,
+        action_space=env.action_space,
+        eps_clip=args.eps_clip,
+        value_clip=args.value_clip,
+        dual_clip=args.dual_clip,
+        advantage_normalization=args.norm_adv,
+        recompute_advantage=args.recompute_adv,
+    ).to(args.device)
+
+    # TODO: Integrate Intrinsic Curiosity
+    # if args.icm_lr_scale > 0:
+    #     feature_net = DQN(
+    #         *args.state_shape, args.action_shape, args.device, features_only=True
+    #     )
+    #     action_dim = np.prod(args.action_shape)
+    #     feature_dim = feature_net.output_dim
+    #     icm_net = IntrinsicCuriosityModule(
+    #         feature_net.net,
+    #         feature_dim,
+    #         action_dim,
+    #         hidden_sizes=[args.hidden_size],
+    #         device=args.device,
+    #     )
+    #     icm_optim = torch.optim.Adam(icm_net.parameters(), lr=args.lr)
+    #     policy = ICMPolicy(
+    #         policy, icm_net, icm_optim, args.icm_lr_scale, args.icm_reward_scale,
+    #         args.icm_forward_loss_weight
+    #     ).to(args.device)
+
+    # load a previous policy
+    if args.resume_path:
+        policy.load_state_dict(torch.load(args.resume_path, map_location=args.device))
+        print("Loaded agent from: ", args.resume_path)
+
+    # replay buffer: `save_last_obs` and `stack_num` can be removed together
+    # when you have enough RAM
+    buffer = VectorReplayBuffer(
+        args.buffer_size,
+        buffer_num=len(train_envs),
+        ignore_obs_next=True,
+        save_only_last_obs=True,
+        stack_num=args.frame_stack,
+    )
+    
+    # collector
+    train_collector = Collector(policy, train_envs, buffer, exploration_noise=True)
+    test_collector = Collector(policy, test_envs, exploration_noise=True)
+
+    # log
+    now = datetime.datetime.now().strftime("%y%m%d-%H%M%S")
+    # args.algo_name = "ppo_icm" if args.icm_lr_scale > 0 else "ppo"
+    args.algo_name = "ppo"
+    log_name = os.path.join(args.task, args.algo_name, str(args.seed), now)
+    log_path = os.path.join(args.logdir, log_name)
+
+    # Logger
+    if args.logger == "wandb":
+        logger = WandbLogger(
+            save_interval=1,
+            name=log_name.replace(os.path.sep, "__"),
+            run_id=args.resume_id,
+            config=args,
+            project=args.wandb_project,
+        )
+    writer = SummaryWriter(log_path)
+    writer.add_text("args", str(args))
+
+    print("Initializing logger")
+    if args.logger == "tensorboard":
+        logger = TensorboardLogger(writer)
+    else:  # wandb
+        logger.load(writer)
+
+    def save_best_fn(policy):
+        torch.save(policy.state_dict(), os.path.join(log_path, "policy.pth"))
+
+    def stop_fn(mean_rewards):
+        if env.spec.reward_threshold:
+            return mean_rewards >= env.spec.reward_threshold
+        else:
+            return False
+
+    def save_checkpoint_fn(epoch, env_step, gradient_step):
+        # see also: https://pytorch.org/tutorials/beginner/saving_loading_models.html
+        ckpt_path = os.path.join(log_path, f"checkpoint_{epoch}.pth")
+        torch.save({"model": policy.state_dict()}, ckpt_path)
+        return ckpt_path
+
+    # watch agent's performance
+    def watch():
+        print("Setup test envs ...")
+        policy.eval()
+        test_envs.seed(args.seed)
+        if args.save_buffer_name:
+            print(f"Generate buffer with size {args.buffer_size}")
+            buffer = VectorReplayBuffer(
+                args.buffer_size,
+                buffer_num=len(test_envs),
+                ignore_obs_next=True,
+                save_only_last_obs=True,
+                stack_num=args.frame_stack,
+            )
+            collector = Collector(policy, test_envs, buffer, exploration_noise=True)
+            result = collector.collect(n_step=args.buffer_size)
+            print(f"Save buffer into {args.save_buffer_name}")
+            # Unfortunately, pickle will cause oom with 1M buffer size
+            buffer.save_hdf5(args.save_buffer_name)
+        else:
+            print("Testing agent ...")
+            test_collector.reset()
+            result = test_collector.collect(
+                n_episode=args.test_num, render=args.render
+            )
+        rew = result["rews"].mean()
+        print(f"Mean reward (over {result['n/ep']} episodes): {rew}")
+
+    if args.watch:
+        watch()
+        exit(0)
+
+    # test train_collector and start filling replay buffer
+    print("Start training ...")
+    train_collector.collect(n_step=args.batch_size * args.training_num)
+    # trainer
+    result = onpolicy_trainer(
+        policy,
+        train_collector,
+        test_collector,
+        args.epoch,
+        args.step_per_epoch,
+        args.repeat_per_collect,
+        args.test_num,
+        args.batch_size,
+        step_per_collect=args.step_per_collect,
+        stop_fn=stop_fn,
+        save_best_fn=save_best_fn,
+        logger=logger,
+        test_in_train=False,
+        resume_from_log=args.resume_id is not None,
+        save_checkpoint_fn=save_checkpoint_fn,
+    )
+
+    pprint.pprint(result)
+    watch()
+
+
+if __name__ == "__main__":
+    test_ppo(get_args())
---
+++ configure_env.py
@@ -0,0 +1,76 @@
+# Code adapted from https://github.com/thu-ml/tianshou/blob/master/examples/atari/atari_wrapper.py
+
+import retro
+import numpy as np
+import gymnasium as gym
+import warnings
+import os
+
+from tianshou.env import SubprocVectorEnv
+
+class MaxAndSkipEnv(gym.Wrapper):
+    """Return only every `skip`-th frame (frameskipping) using most recent raw
+    observations (for max pooling across time steps)
+    :param gym.Env env: the environment to wrap.
+    :param int skip: number of `skip`-th frame.
+    """
+
+    def __init__(self, env, skip=4):
+        super().__init__(env)
+        self._skip = skip
+
+    def step(self, action):
+        """Step the environment with the given action. Repeat action, sum
+        reward, and max over last observations.
+        """
+        obs_list, total_reward = [], 0.
+        new_step_api = False
+        for _ in range(self._skip):
+            step_result = self.env.step(action)
+            if len(step_result) == 4:
+                obs, reward, done, info = step_result
+            else:
+                obs, reward, term, trunc, info = step_result
+                done = term or trunc
+                new_step_api = True
+            obs_list.append(obs)
+            total_reward += reward
+            if done:
+                break
+        max_frame = np.max(obs_list[-2:], axis=0)
+        if new_step_api:
+            return max_frame, total_reward, term, trunc, info
+
+        return max_frame, total_reward, done, info
+
+# Given an environment ID, create a wrapped environment.
+def create_wrapped_env(env_id, grayscale=True, resize_frame=True, normalize=False, frame_skip = 4, frame_stack=4, custom_integration_path=None):
+    # Attempt to integrate new enviornment if specified
+    if custom_integration_path is not None:
+        SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__)) # Directory of the script
+        NEW_INTEGRATIONS_DIR = os.path.join(SCRIPT_DIR, custom_integration_path)
+        retro.data.Integrations.add_custom_path(NEW_INTEGRATIONS_DIR) # Add folder containing new integrations to path.
+    
+    env = retro.make(env_id, inttype=retro.data.Integrations.ALL, use_restricted_actions=retro.Actions.DISCRETE, render_mode="rgb_array")
+
+    if grayscale:
+        env = gym.wrappers.GrayScaleObservation(env)
+    if resize_frame:
+        env = gym.wrappers.ResizeObservation(env, (84, 84))
+    if normalize:
+        env = gym.wrappers.NormalizeObservation(env)
+    if frame_skip:
+        env = MaxAndSkipEnv(env, frame_skip)
+    if frame_stack:
+        env = gym.wrappers.FrameStack(env, frame_stack, False)
+
+    return env
+
+# Make environments for training and testing
+def make_retro_env(task, seed, training_num, test_num, **kwargs):
+    # TODO: envpool integration
+    # env = create_wrapped_env(task, **kwargs)
+    env = SubprocVectorEnv([lambda: create_wrapped_env(task, **kwargs)])
+    # training_envs = SubprocVectorEnv([lambda: create_wrapped_env(task, **kwargs) for _ in range(training_num)])
+    # test_envs = SubprocVectorEnv([lambda: create_wrapped_env(task, **kwargs) for _ in range(test_num)])
+    return env, env, env # return the same environment for training and testing.
