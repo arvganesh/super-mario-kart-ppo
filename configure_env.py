0bdc3445cf1620935ba8e24b400f4c65130ccc66
Arvind Ganesh <arvind.cganesh@gmail.com>
2023-04-04 11:17:01 -0500

    Added more env wrappers

--- configure_env.py
+++ configure_env.py
@@ -8,9 +8,9 @@ import os
 
 from tianshou.env import SubprocVectorEnv, ShmemVectorEnv
 
-class MaxAndSkipEnv(gym.Wrapper):
+class FrameSkipping(gym.Wrapper):
     """Return only every `skip`-th frame (frameskipping) using most recent raw
-    observations (for max pooling across time steps)
+    observations
     :param gym.Env env: the environment to wrap.
     :param int skip: number of `skip`-th frame.
     """
@@ -37,30 +37,86 @@ class MaxAndSkipEnv(gym.Wrapper):
             total_reward += reward
             if done:
                 break
-        max_frame = np.max(obs_list[-2:], axis=0)
         if new_step_api:
-            return max_frame, total_reward, term, trunc, info
+            return obs_list[-1], total_reward, term, trunc, info
+        return obs_list[-1], total_reward, done, info
 
-        return max_frame, total_reward, done, info
+class Discretizer(gym.ActionWrapper):
+    """
+    Wrap a gym environment and make it use discrete actions.
+    Args:
+        combos: ordered list of lists of valid button combinations
+    """
+
+    def __init__(self, env, combos):
+        super().__init__(env)
+        assert isinstance(env.action_space, gym.spaces.MultiBinary)
+        buttons = env.unwrapped.buttons
+        self._decode_discrete_action = []
+        for combo in combos:
+            arr = np.array([False] * env.action_space.n)
+            for button in combo:
+                arr[buttons.index(button)] = True
+            self._decode_discrete_action.append(arr)
+
+        self.action_space = gym.spaces.Discrete(len(self._decode_discrete_action))
+
+    def action(self, act):
+        return self._decode_discrete_action[act].copy()
+
+class SimpleDrivingDiscrtizer(Discretizer):
+    """
+    Simplify the action space for driving in Mario Kart. Used as a baseline.
+    From 468 SNES Operations -> 6 actions.
+    """
+    def __init__(self, env):
+        super().__init__(env=env, combos=[
+            [], # Do nothing
+            ['LEFT'], # Turn left
+            ['RIGHT'], # Turn right
+            ['B'], # Accelerate
+            ['LEFT', 'B'], # Accelerate and turn left
+            ['RIGHT', 'B'], # Accelerate and turn right
+        ])
+
+class RoadFocusWrapper(gym.ObservationWrapper):
+    """
+    Remove mini-map from observations.
+    """
+    def __init__(self, env):
+        super().__init__(env=env)
+        assert(isinstance(self.observation_space, gym.spaces.Box))
+        assert(len(self.observation_space.shape) == 2)
+        self.start_row = 25 # Top 25 rows of pixels include timer.
+        self.end_row = self.observation_space.shape[0] // 2 - 10 # - 10 to remove black bars from the bottom rows.
+        num_rows = self.end_row - self.start_row
+        self.observation_space = gym.spaces.Box(low=0, high=225, shape=(num_rows, self.observation_space.shape[1]), dtype=np.uint8)
+
+    def observation(self, obs):
+        return obs[self.start_row : self.end_row, :]
 
 # Given an environment ID, create a wrapped environment.
-def create_wrapped_env(env_id, grayscale=True, resize_frame=True, normalize=True, frame_skip = 4, frame_stack=4, max_episode_steps=1000, custom_integration_path=None):
+def create_wrapped_env(env_id, simple_action_space=True, simple_obs_space=True, grayscale=True, resize_frame=True, normalize=True, frame_skip = 4, frame_stack=4, max_episode_steps=1000, custom_integration_path=None):
     # Attempt to integrate new enviornment if specified
     if custom_integration_path is not None:
         SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__)) # Directory of the script
         NEW_INTEGRATIONS_DIR = os.path.join(SCRIPT_DIR, custom_integration_path)
         retro.data.Integrations.add_custom_path(NEW_INTEGRATIONS_DIR) # Add folder containing new integrations to path.
     
-    env = retro.make(env_id, inttype=retro.data.Integrations.ALL, use_restricted_actions=retro.Actions.DISCRETE, render_mode="rgb_array")
+    env = retro.make(env_id, inttype=retro.data.Integrations.ALL, render_mode="rgb_array")
 
     if grayscale:
         env = gym.wrappers.GrayScaleObservation(env)
+    if simple_obs_space:
+        env = RoadFocusWrapper(env)
     if resize_frame:
         env = gym.wrappers.ResizeObservation(env, (84, 84))
-    # if normalize:
+    # if normalize: currently directly normalization when forwarding in model
     #     env = gym.wrappers.NormalizeObservation(env)
+    if simple_action_space:
+        env = SimpleDrivingDiscrtizer(env)
     if frame_skip:
-        env = MaxAndSkipEnv(env, frame_skip)
+        env = FrameSkipping(env, frame_skip)
     if frame_stack:
         env = gym.wrappers.FrameStack(env, frame_stack, False)
     if max_episode_steps:
@@ -77,4 +133,4 @@ def make_retro_env(task, seed, training_num, test_num, **kwargs):
     # training_envs = DummyVectorEnv([lambda: create_wrapped_env(task, **kwargs) for _ in range(training_num)])
     # test_envs = DummyVectorEnv([lambda: create_wrapped_env(task, **kwargs) for _ in range(training_num)])
 
-    return training_envs, training_envs
+    return training_envs, test_envs
